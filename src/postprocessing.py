2e47b3f18b00de529159612dfceac46887f96599
Myrthe <myrtilleb2@gmail.com>
2023-02-13 09:57:58 +0000

    Separate pre- and post processing of EMG data

---
+++ src/postprocessing.py
@@ -0,0 +1,79 @@
+"""
+ * @author Myrthe Tilleman
+ * @email mtillerman@ossur.com
+ * @create date 2023-02-13 09:01:22
+ * @desc [description]
+"""
+
+import pandas as pd
+
+from preprocessing import (define_dominant_muscle, define_level,
+                           normalise_data_MVC, normalise_data_RMS,
+                           threshold_reached)
+
+
+def extract_data(filename, verbose=True, comma=False):
+    """
+    Extracts data and returns a reshaped data frame with each variable type
+    in a different column, ordered by timestamp.
+
+    Args:
+        filename (data frame): csv file with EMG data recorded using Ossur
+        Toolbox.
+        verbose (bool, optional): _description_. Defaults to True.
+        comma (bool, optional): Whether decimals are denoted after a comma.
+        Defaults to False.
+
+    Returns:
+        data frame
+    """
+    if comma:
+        dec = ','
+    else:
+        dec = '.'
+    df = pd.read_csv(filename, delimiter=';', decimal=dec, header=0, quoting=3)
+    print(df)
+    df = df.pivot(columns="variableType", values="numValue", index="timestamp")
+    df = df.reset_index(level=0)
+
+    return df
+
+
+if __name__ == "__main__":
+    vib_emg = False
+    level = None
+    columns = [
+        'timestamp',
+        'BASE_ACTIVITY', 'BASE_GAIT_PHASE',
+        'BSMB_MUSCLE_EXTEND', 'BSMB_MUSCLE_FLEX']
+    temp = pd.DataFrame(columns=columns)
+
+    folder = "C:/Users/mtillerman/OneDrive - Ossur hf/Documents/EMG_data/"
+    # data_file = "20230111151536909_210000.8.Variables.csv"
+    data_file = "ramp descend.csv"
+    # data_file = "signal check sitting.csv"
+    # data_file = "stairs prev settings.csv"
+    data = extract_data(folder + data_file)
+
+    # select columns to process
+    data = data[columns]
+    print(data.head())
+
+    for _, row in data.iterrows():  # loop through data as if live data
+        row = row.to_frame().T
+        temp = pd.concat([temp, row], ignore_index=True)
+        if len(temp) > 100:  # take out first line
+            temp.drop(axis=0, index=0, inplace=True)
+
+        # TODO subtract 'rest' activity
+        normal = normalise_data_MVC(row)
+        print(normal)
+        # normal = normalise_data_RMS(temp)
+        print(normal['BSMB_MUSCLE_EXTEND'])
+        vib_emg = threshold_reached(normal)
+
+        if vib_emg:
+            level_l = define_level(normal)
+            level_m = define_dominant_muscle(normal)
+            if level_l != level_m:
+                print(level_l, level_m)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,39 +5,11 @@
  * @desc Process EMG data from file: normalise and define activation level
 """
 
-import pandas as pd
-
-from utils import EXTEND, FLEX, LEVELS, MVC
+from utils import LEVELS, MVC
 
 
-def extract_data(filename, verbose=True, comma=False):
-    """
-    Extracts data and returns a reshaped data frame with each variable type
-    in a different column, ordered by timestamp.
-
-    Args:
-        filename (data frame): csv file with EMG data recorded using Ossur
-        Toolbox.
-        verbose (bool, optional): _description_. Defaults to True.
-        comma (bool, optional): Whether decimals are denoted after a comma.
-        Defaults to False.
-
-    Returns:
-        data frame
-    """
-    if comma:
-        dec = ','
-    else:
-        dec = '.'
-    df = pd.read_csv(filename, delimiter=';', decimal=dec, header=0, quoting=3)
-    print(df)
-    df = df.pivot(columns="variableType", values="numValue", index="timestamp")
-    df = df.reset_index(level=0)
-
-    return df
-
-
-def normalise_data_RMS(data_frame):
+def normalise_data_RMS(data_frame, extend='BSMB_MUSCLE_EXTEND',
+                       flex='BSMB_MUSCLE_FLEX'):
     """
     Take the root mean square and normalises data according to 40% of the
     maximum voluntary contraction (MVC).
@@ -49,17 +21,18 @@ def normalise_data_RMS(data_frame):
         data frame: with 1 row with the normalised EMG data
     """
     normalised = data_frame.iloc[-1]
-    RMS_window = data_frame[[EXTEND, FLEX]]
+    RMS_window = data_frame[[extend, flex]]
 
     square = RMS_window.pow(2)
     rms = square.mean(axis=0).pow(0.5) / (0.4 * MVC)
 
-    normalised[EXTEND] = rms[EXTEND]
-    normalised[FLEX] = rms[FLEX]
+    normalised[extend] = rms[extend]
+    normalised[flex] = rms[flex]
     return normalised
 
 
-def normalise_data_MVC(data_frame):
+def normalise_data_MVC(data_frame, extend='BSMB_MUSCLE_EXTEND',
+                       flex='BSMB_MUSCLE_FLEX'):
     """
     Normalises data according to 40% of the maximum voluntary contraction (MVC)
 
@@ -71,14 +44,15 @@ def normalise_data_MVC(data_frame):
     """
     normalised = data_frame.reset_index(drop=True)
 
-    for muscle in [EXTEND, FLEX]:
+    for muscle in [extend, flex]:
         emg_signal = data_frame[muscle].reset_index(drop=True)
         normalised[muscle] = emg_signal / (0.4 * MVC[muscle])
 
     return normalised
 
 
-def threshold_reached(data_frame, vib_emg=False):
+def threshold_reached(data_frame, vib_emg=False, extend='BSMB_MUSCLE_EXTEND',
+                      flex='BSMB_MUSCLE_FLEX'):
     """ Sets vib_emg to True when the EMG threshold is reached, EMG > 0.1.
 
     Args:
@@ -89,12 +63,13 @@ def threshold_reached(data_frame, vib_emg=False):
         bool: is True when EMG threshold is reached and feedback should be
         activated.
     """
-    if (data_frame[EXTEND] > 0.1) | (data_frame[FLEX] > 0.1):
+    if (data_frame[extend] > 0.1) | (data_frame[flex] > 0.1):
         vib_emg = True
     return vib_emg
 
 
-def define_dominant_muscle(data_frame):
+def define_dominant_muscle(data_frame, extend='BSMB_MUSCLE_EXTEND',
+                           flex='BSMB_MUSCLE_FLEX'):
     """
     Defines vibrator level based on muscle activation based on the difference
     in activation between the extensor and the flexor.
@@ -108,7 +83,7 @@ def define_dominant_muscle(data_frame):
         int: level, ranging from -4 to 4, -4 = extensor min & flexor max,
         0 = equal contracted, 4 = extensor max & flexor min
     """
-    dominant_muscle = data_frame[EXTEND] - data_frame[FLEX]
+    dominant_muscle = data_frame[extend] - data_frame[flex]
     thresholds = [-0.65, -0.4, -0.2, -0.1, 0.1, 0.2, 0.4, 0.65]  # from Tchimino, 2022
     if dominant_muscle <= thresholds[0]:  # smaller than -0.65
         level = LEVELS[0]
@@ -122,7 +97,8 @@ def define_dominant_muscle(data_frame):
     return level
 
 
-def define_level(data_frame):
+def define_level(data_frame, extend='BSMB_MUSCLE_EXTEND',
+                 flex='BSMB_MUSCLE_FLEX'):
     """
     Defines vibrator level based on muscle activation. First, the level is
     defined for each muscle. The final level is the difference between
@@ -139,58 +115,18 @@ def define_level(data_frame):
     thresholds = [0.0, 0.1, 0.2, 0.4, 0.65]  # levels 0-4 from Tchimino '22
     level_extend = None
     level_flex = None
-    # print(data_frame[[EXTEND, FLEX]])
+    # print(data_frame[[extend, flex]])
     for i, t in enumerate(thresholds[:-1]):
-        if data_frame[EXTEND] >= thresholds[-1]:  # larger than 0.65
+        if data_frame[extend] >= thresholds[-1]:  # larger than 0.65
             level_extend = 4
-        elif t <= data_frame[EXTEND] < thresholds[i + 1]:
+        elif t <= data_frame[extend] < thresholds[i + 1]:
             level_extend = i
 
-        if data_frame[FLEX] >= thresholds[-1]:  # larger than 0.65
+        if data_frame[flex] >= thresholds[-1]:  # larger than 0.65
             level_flex = 4
-        elif t <= data_frame[FLEX] < thresholds[i + 1]:
+        elif t <= data_frame[flex] < thresholds[i + 1]:
             level_flex = i
 
-    print(data_frame[EXTEND], data_frame[FLEX])
+    print(data_frame[extend], data_frame[flex])
     level = level_extend - level_flex
     return level
-
-
-if __name__ == "__main__":
-    vib_emg = False
-    level = None
-    columns = [
-        'timestamp',
-        'BASE_ACTIVITY', 'BASE_GAIT_PHASE',
-        'BSMB_MUSCLE_EXTEND', 'BSMB_MUSCLE_FLEX']
-    temp = pd.DataFrame(columns=columns)
-
-    folder = "C:/Users/mtillerman/OneDrive - Ossur hf/Documents/EMG_data/"
-    # data_file = "20230111151536909_210000.8.Variables.csv"
-    data_file = "ramp descend.csv"
-    # data_file = "signal check sitting.csv"
-    # data_file = "stairs prev settings.csv"
-    data = extract_data(folder + data_file)
-
-    # select columns to process
-    data = data[columns]
-    print(data.head())
-
-    for _, row in data.iterrows():  # loop through data as if live data
-        row = row.to_frame().T
-        temp = pd.concat([temp, row], ignore_index=True)
-        if len(temp) > 100:  # take out first line
-            temp.drop(axis=0, index=0, inplace=True)
-
-        # TODO subtract 'rest' activity
-        normal = normalise_data_MVC(row)
-        print(normal)
-        normal = normalise_data_RMS(temp)
-        print(normal)
-        # vib_emg = threshold_reached(normal)
-
-        if vib_emg:
-            level_l = define_level(normal)
-            level_m = define_dominant_muscle(normal)
-            if level_l != level_m:
-                print(level_l, level_m)
