697d2723624e821781b5d95a1ea9fffeb9741106
Myrthe <myrtilleb2@gmail.com>
2023-02-14 10:17:48 +0000

    Restructure reading uart to class

--- src/read_uart.py
+++ src/read_uart.py
@@ -10,75 +10,108 @@ import struct
 import board
 import busio
 
-from preprocessing import normalise_data_MVC, define_dominant_muscle
+from preprocessing import define_dominant_muscle, normalise_data_MVC
 from utils import get_MVC
 
-gc.collect()
-start_mem = gc.mem_free()
-print('start memory', start_mem)
 
-uart = busio.UART(board.TX, board.RX, baudrate=921600)  # 1000000)
+class ReadUart():
+    def __init__(self, num_variables=2, array_length=14, baud_rate=921600,
+                 data_num_bytes=2, start_num_bytes=8):
+        self.num_variables = num_variables  # EMG_Flex, EMG_Extend
+        self.array_length = array_length  # num bytes to read at a time
+        self.baud_rate = baud_rate
+        self.data_num_bytes = data_num_bytes
+        self.start_num_bytes = start_num_bytes  # bytes before EMG data
+        self.starting_byte = b'\xaa\n\xb1'
 
-num_variables = 2  # EMG_Flex, EMG_Extend
-data_num_bytes = 2
-start_num_bytes = 8
-array_length = 14
-raw_data = bytearray(array_length)
-all_data = []
-if data_num_bytes == 2:
-    data_type = 'h'     # 2 byte integer
-elif data_num_bytes == 4:
-    data_type = 'f'     # 4 byte float
+        if self.data_num_bytes == 2:
+            self.data_type = 'h'     # 2 byte integer
+        elif self.data_num_bytes == 4:
+            self.data_type = 'f'     # 4 byte float
 
-mvc = get_MVC('MVC.csv')
+    def initialise_uart(self):
+        """ Initialise UART port and set baud rate.
+        Read up to array_length bytes at a time.
+        """
+        self.uart = busio.UART(board.TX, board.RX, baudrate=self.baud_rate)
+        self.raw_data = bytearray(self.array_length)
 
-x = 0
+    def get_serial_data(self):
+        """ Reads array_length bytes. Then checks whether the starting byte
+        is present and reads additional bytes, so the whole byte array is the
+        correct length.
 
-extend = 1
-flex = 0
+        Returns:
+            bytearray: array of array_length bytes starting with the start byte
+        """
+        self.uart.readinto(self.raw_data)
+        start_index = self.raw_data.find(self.starting_byte)  # find start byte
+        private_data = self.raw_data[start_index:]
 
-gc.collect()
-intialised_mem = gc.mem_free()
-print('initialised memory', intialised_mem)
-print(mvc)
+        read_next = self.uart.read(start_index)  # read extra bytes
+        private_data += bytearray(read_next)  # add extra bytes to the end
+        return private_data
 
-while True:
-    # emg_data = uart.read(14)  # read up to 32 bytes
-    # print(emg_data)  # this is a bytearray type
-    uart.readinto(raw_data)
-    start_index = raw_data.find(b'\xaa\n\xb1')  # find starting byte
-    private_data = raw_data[start_index:]
-    # private_data = copy.deepcopy(raw_data[:])
-    read_next = uart.read(start_index)  # read extra bytes if necessary
-    private_data += bytearray(read_next)  # add extra bytes to make length 14
-    # print(raw_data)
-    # print(read_next)
-    # print(private_data)
+    def extract_emg_data(self, private_data):
+        """ Extracts the EMG data from the byte array and converts the bytes
+        to integers.
 
-    x += 1
+        Args:
+            private_data (bytearray): byte array of array_length
 
-    emg_data = [private_data[start_num_bytes + i * data_num_bytes:
-                start_num_bytes + data_num_bytes + i * data_num_bytes]
-                for i in range(num_variables)]
+        Returns:
+            _type_: _description_
+        """
+        emg_data = [
+            private_data[self.start_num_bytes + i * self.data_num_bytes:
+                         self.start_num_bytes + self.data_num_bytes +
+                         i * self.data_num_bytes]
+            for i in range(self.num_variables)]
 
-    # convert bytes to int
-    emg_value = [struct.unpack(data_type, data)[0] for data in emg_data]
-    print(emg_value)
-    normal = normalise_data_MVC(emg_value, mvc, extend, flex)
-    print(normal)
-    level = define_dominant_muscle(normal, extend, flex)
-    print(level)
-    all_data.append(emg_value)
-    # print(emg_value)
+        # convert bytes to int
+        emg_value = [struct.unpack(self.data_type, data)[0]
+                     for data in emg_data]
+        return emg_value
 
-    if x == 100:  # stop
-        break
+
+if __name__ == '__main__':
+    gc.collect()
+    start_mem = gc.mem_free()
+    print('start memory', start_mem)
+
+    read_uart = ReadUart()
+    read_uart.initialise_uart()
+    mvc = get_MVC('MVC.csv')
+
+    x = 0
+
+    extend = 1
+    flex = 0
 
     gc.collect()
-    # loop_mem = gc.mem_free()
-    # print('loop memory', x, loop_mem)
+    intialised_mem = gc.mem_free()
+    print('initialised memory', intialised_mem)
+    print(mvc)
 
-print(all_data)
-gc.collect()
-end_mem = gc.mem_free()
-print('end memory', end_mem)
+    while True:
+        data = read_uart.get_serial_data()
+        emg_value = read_uart.extract_emg_data(data)
+
+        x += 1
+
+        print(emg_value)
+        normal = normalise_data_MVC(emg_value, mvc, extend, flex)
+        print(normal)
+        level = define_dominant_muscle(normal, extend, flex)
+        print(level)
+
+        if x == 100:  # stop
+            break
+
+        gc.collect()
+        # loop_mem = gc.mem_free()
+        # print('loop memory', x, loop_mem)
+
+    gc.collect()
+    end_mem = gc.mem_free()
+    print('end memory', end_mem)
