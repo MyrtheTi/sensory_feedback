7ea2e9b54697196992332190e3ffa1690e8a01e8
Myrthe <myrtilleb2@gmail.com>
2023-02-28 15:56:14 +0000

    Feedback calibration - Request user input after each stimulation

--- src/stimulation_calibration.py
+++ src/stimulation_calibration.py
@@ -6,78 +6,57 @@
  just noticeable difference.
 """
 
-import time
-import board
-import pwmio
 import math
+import time
 
-from vibrators_PWM import duty_cycle_value
-
+from activate_vibration_motors import ActivateVibrationMotor
 
 if __name__ == '__main__':
-    n_of_vibrators = 4
-    output_start = 7
     calibration = {}
 
+    motors = ActivateVibrationMotor()
+    levels = []
     # define comfortable range, see Chee et al. 2022
-    for v in range(output_start, output_start + n_of_vibrators):
-        output = 'D' + str(v)
-        vibrator = pwmio.PWMOut(board.str(output))
+    for vibrator_level in motors.level_list:  # loop through levels
+        print('level', vibrator_level)
+        motors.set_motor_value(vibrator_level["PIN"], False)  # turn off
 
-        min = [0, 0, 0, 0, 0]
-        max = [1, 1, 1, 1, 1]
-        # repeat the calibration 5 times
-        for i in range(5):
+        thresholds = [0, 0, 0, 0, 0]
+        for repeat in range(5):  # repeat the calibration 5 times
             # pause before continuing
             # TODO look up how to use inputs in circuit python
             input(
                 'Press a key if you are ready to start the next calibration\n')
 
-            # 1:
             # report starting to perceive something clear
             # (perceptual threshold) at an intensity of 2/10
-            # 2:
-            # report whenever the intensity began to be come stronger
-            # (remaining below an uncomfortable level) at an intensity of 8/10
 
-            # increase the vibration intensity by 1%, activate 2 s, pause 1 s
-            for intensity in range(0, 1, 0.01):
+            for i in range(1, 10):  # increase duration by 1ms
+                on_time = i / 1000  # s on
+                print(on_time, 's')
+                vibrator_level["VIBRATION_TIME"] = on_time
 
-                vibrator.duty_cycle = duty_cycle_value(intensity)
-                time.sleep(2)
-                vibrator.duty_cycle = 0
-                time.sleep(1)
-                if input == 'l':  # input for lower threshold, maybe easier in PsychoPy or so?
-                    min[i] = intensity
-                elif input == 'h':  # input for high threshold
-                    max[i] = intensity
+                start = time.monotonic()
+                while time.monotonic() - start < 2:  # activate for 2 s
+                    now = time.monotonic()
+                    motors.check_time_to_change(vibrator_level, now)
+
+                motors.set_motor_value(vibrator_level["PIN"], False)  # turn off
+                user_input = input('press a key if you felt the vibration with an intensity of 2/10')
+                if user_input != '':
+                    thresholds[repeat] = on_time
                     break
+                time.sleep(1)  # pause for 1 second
 
-        # take the average
-        calibration[str(vibrator) + '_min'] = math.avg(min)
-        calibration[str(vibrator) + '_max'] = math.avg(max)
+        levels.append({"LEVEL": vibrator_level["LEVEL"], "thresholds": thresholds})
+        print(levels)
+        # # take the average
+        # calibration[str(vibrator) + '_min'] = math.avg(min)
+        # calibration[str(vibrator) + '_max'] = math.avg(max)
 
         # TODO save these values in a file
 
-# define just noticeable difference for each vibrating motor;
-# TODO maybe do not use stimulation levels?
-# but just located stimulation for encoding EMG signals? to keep it more simple?
-# start with the minimum value (reference) and minimum + 40 % (test)
-# stimulate twice, 1 reference, 1 test, randomise the order
-# stimulus 2 s with 1 s pause in between
-# participant says which one is higher
-# if incorrect, increase the test with 5%
-# if correct, the test becomes the new reference
-# continue until test would be above maximum
-# repeat 5 times
-# define the total number of levels
-
-# validate levels for each vibrating motor
-# present 2 signals of different or the same levels
-# participant says which one is higher or the same
-# if > 75% correct, validation is good, otherwise,
-# JND calibration needs to be redefined
-
-# also test whether participants can differentiate between the vibrators
+# add familiarisation period
+# also validate whether participants can differentiate between the vibrators
 # see Chee et a. 2022 - Optimally-calibrated non-invasive...
 # or as in Tchimino et al. 2022; > 90% is correctly validated here
