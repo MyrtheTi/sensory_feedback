9e88e11acbcc0135a6942e296c30585c807ca672
Myrthe Tilleman <myrtilleb2@gmail.com>
2023-05-17 11:58:32 +0300

    Plot confusion matrices for combined data - change input to list of users & dates

--- src/plot_results.py
+++ src/plot_results.py
@@ -14,22 +14,30 @@ from utils import read_file
 def plot_confusion_matrix(user, date):
     """ Plots the confusion matrices of the validation using all levels and
     when separated in 3 levels (flexion, co-contraction, and extension).
+    Combines the results of users/dates if multiple are given.
 
     Args:
-        user (str): User name / number of the validation files.
-        date (str): Date of the validation files.
+        user (list of str): User names / number of the validation files.
+        date (list of str): Dates of the validation files.
     """
-    path = f'user_files/{user}/{date}/'
+    true_l = []
+    predicted_l = []
 
-    true_labels = read_file(path, 'true_labels.csv', ['int'])[0]
-    predicted_labels = read_file(path, 'predicted_labels.csv', ['int'])[0]
+    for u, d in zip(user, date):
+        path = f'user_files/{u}/{d}/'
+
+        true_labels = read_file(path, 'true_labels.csv', ['int'])[0]
+        predicted_labels = read_file(path, 'predicted_labels.csv', ['int'])[0]
+
+        true_l.extend(true_labels)
+        predicted_l.extend(predicted_labels)
 
     fig, axes = plt.subplots(ncols=2, figsize=(20, 10))
     titles_options = [
-        ("Confusion matrix with all levels", true_labels, predicted_labels,
+        ("Confusion matrix with all levels", true_l, predicted_l,
          None),
-        ("Confusion matrix with combined levels", converge_levels(true_labels),
-         converge_levels(predicted_labels),
+        ("Confusion matrix with combined levels", converge_levels(true_l),
+         converge_levels(predicted_l),
          ['flexion', 'co-contraction', 'extension'])
     ]
 
@@ -74,7 +82,7 @@ def converge_levels(labels):
 
 
 if __name__ == '__main__':
-    user = 'me'
-    date = '2023_03_15'
+    user = ['U401', 'U412', 'U747']
+    date = ['2023_04_25', '2023_04_26', '2023_05_03']
 
     plot_confusion_matrix(user, date)
