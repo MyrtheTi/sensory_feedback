bea5daaea931ee775c9935fa9fd9aebcae88fcc4
Myrthe Tilleman <myrtilleb2@gmail.com>
2023-07-04 12:59:18 +0300

    Plot difference and vibration levels in one plot with coloured blocks for feedback levels

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -6,6 +6,7 @@
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import tikzplotlib
 
@@ -56,33 +57,71 @@ def visualise_data(raw_data, normal_data, data_file,
         the raw EMG data. Defaults to True.
     """
     if all:
-        data_list = [raw_data, normal_data, normal_data, normal_data]
-        y_list = [[extend, flex], [extend, flex], 'difference', 'LEVEL']
+        THRESHOLDS = [-1, -0.65, -0.4, -0.2, -0.1, 0.1, 0.2, 0.4, 0.65, 1]
+
+        text = ['Level -4', 'Level -3', 'Level -2', 'Level -1', 'Level 0',
+                'Level 1', 'Level 2', 'Level 3', 'Level 4']
+        data_list = [raw_data, normal_data, normal_data]
+        y_list = [[extend, flex], [extend, flex],
+                  ['difference', 'threshold level']]
         y_label_list = ['Raw EMG data (V)', 'Normalised EMG data',
-                        'Normalised extension - flexion', 'Vibration level']
+                        'Normalised extension - flexion']
         label_list = [['Extension muscle', 'Flexion muscle'],
                       ['Extension muscle', 'Flexion muscle'],
-                      'Extension - Flexion', 'Feedback level']
-        name_list = ['raw', 'normal', 'difference', 'level']
+                      ['Extension - Flexion', 'Feedback level']]
+        name_list = ['raw', 'normal', 'difference_level']
+        color_blocks = ['', 'deadzone', 'levels']
         normal_data['difference'] = normal_data[extend] - normal_data[flex]
     else:
         data_list = [raw_data]
         y_list = [[extend, flex]]
         y_label_list = ['Raw EMG data (V)']
         label_list = [['Extension muscle', 'Flexion muscle']]
+        color_blocks = ['']
         name_list = ['raw']
 
-    for data, y, y_label, label, name in zip(data_list, y_list, y_label_list,
-                                             label_list, name_list):
+    for data, y, y_label, label, color, name in zip(
+            data_list, y_list, y_label_list, label_list, color_blocks,
+            name_list):
         fig, ax1 = plt.subplots(figsize=(15, 7))
+
+        if color == 'deadzone':
+            ax1.axhspan(-0.1, 0.1, facecolor='grey', alpha=0.5)
+            ax1.text(normal_data['timestamp'].iloc[1], 0.01, 'Dead zone')
+        elif color == 'levels':
+            ax1.hlines(THRESHOLDS[1:-1], 0, normal_data['timestamp'].iloc[-1],
+                       color='black')
+            ax1.set_yticks(THRESHOLDS)
+            ax1.set_ylim(-1, 1)
+            threshold_middle = []
+            for i, t in enumerate(THRESHOLDS[:-1]):  # place text
+                middle = np.mean([t, THRESHOLDS[i + 1]])
+                threshold_middle.append(middle)
+                ax1.text(normal_data['timestamp'].iloc[1],
+                         middle - 0.01, text[i])
+                if middle < 0:  # flexion - orange
+                    ax1.axhspan(t, THRESHOLDS[i + 1], facecolor='orange',
+                                alpha=0.2)
+                elif middle > 0:  # extension
+                    ax1.axhspan(t, THRESHOLDS[i + 1], facecolor='blue',
+                                alpha=0.2)
+                else:  # co-contraction
+                    ax1.axhspan(t, THRESHOLDS[i + 1], facecolor='grey',
+                                alpha=0.2)
+            threshold_levels = [
+                threshold_middle[int(t + 4)] if not np.isnan(t)
+                else np.nan for t in normal_data.loc[:, 'LEVEL']]
+            normal_data['threshold level'] = threshold_levels
 
         data.plot(kind='line', x='timestamp', y=y, ax=ax1, label=label)
         ax1.set_ylabel(y_label)
         ax1.set_xlabel('Time (s)')
+        ax1.set_xlim(normal_data['timestamp'].iloc[0],
+                     normal_data['timestamp'].iloc[-1])
+        ax1.legend(loc='upper left')
         plt.grid()
 
         tikzplotlib_fix_ncols(fig)
-
         file_name = data_file.split('.')[0] + f'_{name}.tex'
         tikzplotlib.save(f'user_files/results/{file_name}')
         plt.show()
@@ -145,11 +184,11 @@ def simulate_online(user, emg_folder, data_folder, data_file,
 
 
 if __name__ == "__main__":
-    user = 'U493'
-    data_folder = '2023_03_27'
-    emg_calibration = '2023_03_27'
-    data_file = 'sEMG_feedback_power_ankle_log.csv'
-    from_log = True
+    user = 'U401'
+    data_folder = '2023_03_17'
+    emg_calibration = '2023_03_17'
+    data_file = 'plantar_flexion.csv'
+    from_log = False
 
     raw, normal = simulate_online(user, emg_calibration, data_folder,
                                   data_file, from_log=from_log)
