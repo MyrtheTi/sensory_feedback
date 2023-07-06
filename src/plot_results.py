9f7fb7d397f1903855ebc9846da50937bf215e67
Myrthe Tilleman <myrtilleb2@gmail.com>
2023-07-06 12:05:17 +0300

    Plot confusion matrices one by one and adjust font size

--- src/plot_results.py
+++ src/plot_results.py
@@ -2,7 +2,7 @@
  * @author Myrthe Tilleman
  * @email mtillerman@ossur.com
  * @create date 2023-03-07 15:50:32
- * @desc Script to plot the results from the validation
+ * @desc Script to plot the results from the feedback validation
 """
 
 import matplotlib.pyplot as plt
@@ -32,32 +32,41 @@ def plot_confusion_matrix(user, date):
         true_l.extend(true_labels)
         predicted_l.extend(predicted_labels)
 
-    fig, axes = plt.subplots(ncols=2, figsize=(20, 10))
     titles_options = [
-        ("Confusion matrix with all levels", true_l, predicted_l,
-         None),
-        ("Confusion matrix with combined levels", converge_levels(true_l),
+        ("all levels", true_l, predicted_l, None),
+        ("combined levels", converge_levels(true_l),
          converge_levels(predicted_l),
          ['flexion', 'co-contraction', 'extension'])
     ]
 
-    for (title, true, predicted, labels), ax in zip(
-            titles_options, axes.flatten()):
-        ConfusionMatrixDisplay.from_predictions(
+    fs = 20
+    for title, true, predicted, labels in titles_options:
+        fig, ax = plt.subplots(figsize=(20, 10))
+        plt.rc('font', size=fs)
+        ax.tick_params(axis='x', labelsize=fs)
+        ax.tick_params(axis='y', labelsize=fs)
+        cmp = ConfusionMatrixDisplay.from_predictions(
             true,
             predicted,
             cmap=plt.cm.Blues,
             normalize='true',
             ax=ax,
-            labels=labels
+            labels=labels,
+            colorbar=False
         )
-        ax.set_title(title)
+        if 'all' in title:
+            pass
+        else:
+            ax.set_yticks(ax.get_yticks(), ax.get_yticklabels(), rotation=90, va='center')
+            cax = fig.add_axes([ax.get_position().x1+0.01, ax.get_position().y0, 0.02, ax.get_position().height])
+            plt.colorbar(cmp.im_,  cax=cax, cmap=plt.cm.Blues)
         accuracy = accuracy_score(true, predicted)
 
-        ax.set_ylabel('True level')
-        ax.set_xlabel(f'Predicted level\naccuracy={accuracy:0.4f}')
-    plt.tight_layout()
-    plt.show()
+        ax.set_ylabel('True level', size=fs+6)
+        ax.set_xlabel('Predicted level', size=fs+6)
+        filename = f'Confusion_matrix_{title}_{len(user) if len(user) > 0 else user}_user_{accuracy:0.4f}'
+        plt.savefig(f'user_files/results/{filename}.pdf')
+        plt.show()
 
 
 def converge_levels(labels):
